3fa189b8b9274f142d5110a30c347880b9ba67b7
Fercak Florian <fercak.florian@hotmail.fr>
2022-09-15 13:41:53 +0200

    Utilisation d'une variable de classe et d'une methode de classe pour stocker les informations provenant des 3 repository

--- src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
@@ -6,16 +6,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.safetynet.safetynetalerts.repository.GlobalRepository;
+import com.safetynet.safetynetalerts.repository.IGlobalRepository;
 
 @SpringBootApplication
 public class SafetyNetAlertsApplication {
+	
+	private static IGlobalRepository globalRepository;
+	public static IGlobalRepository getGlobalRepository() {
+		return globalRepository;
+	}
 
 	public static void main(String[] args) throws IOException {
+		globalRepository = new GlobalRepository();
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
 		// FireStationRepositoryImpl fireStationRepositoryImpl = new
 		// FireStationRepositoryImpl();
 		// fireStationRepositoryImpl.addFireStation();
-		GlobalRepository globalRepository = new GlobalRepository();
 	}
 
+	public GlobalRepository methodeVariableGlobale() throws IOException  {
+		GlobalRepository globalRepository = new GlobalRepository();
+		return globalRepository;
+	}
 }
---
+++ src/main/java/com/safetynet/safetynetalerts/repository/IGlobalRepository.java
@@ -0,0 +1,11 @@
+package com.safetynet.safetynetalerts.repository;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.safetynet.safetynetalerts.model.Person;
+
+public interface IGlobalRepository {
+	List<Person> getPersonsCoveredByAFirestation(int stationNumber) throws IOException;
+	IPersonRepository getPersonRepository();
+}
