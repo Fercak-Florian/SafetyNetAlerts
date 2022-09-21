ecd45ef6b3ea4ecd8ad2a37cb1cc5a4cece3fa96
Fercak Florian <fercak.florian@hotmail.fr>
2022-09-21 17:06:28 +0200

    test unitaire et integration ok pour GET de FireStationController

---
+++ src/test/java/com/safetynet/safetynetalerts/controller/FireStationControllerTest.java
@@ -0,0 +1,33 @@
+package com.safetynet.safetynetalerts.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.safetynet.safetynetalerts.service.FireStationServiceImpl;
+
+@WebMvcTest(controllers = FireStationController.class)
+public class FireStationControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockBean
+	private FireStationServiceImpl fireStationService;
+
+	@Test
+	public void testGetFireStationFromService() throws Exception {
+		mockMvc.perform(get("/firestations")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testGetFireStationFromServiceWithBadRequest() throws Exception {
+		mockMvc.perform(get("/fire")).andExpect(status().isNotFound());
+	}
+}
